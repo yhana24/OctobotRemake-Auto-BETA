eab9ca99e39818450cd7c8af06626ab8d2117d62
yhana24 <armiayasis24@gmail.com>
2024-12-29 22:30:06 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,7 +4,7 @@ module.exports = {
     credits: "alvarez yhana",
     cooldown: 16,		
     execute(api, event, args, commands) {
-        let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 by Yhana. 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
+        let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 by Yhana. 𝗙𝗮𝗶𝗿𝘆 𝗔𝘃𝗮𝗶𝗯𝗹𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
         helpMessage += '💮═══════════════💮\n';
         commands.forEach((command, name) => {
             helpMessage += `𝙽𝚊𝚖𝚎: ${name}\n`;
@@ -15,7 +15,7 @@ module.exports = {
             helpMessage += `Cooldown: ${command.cooldown !== undefined ? command.cooldown : 'No cooldown'}\n`;
             helpMessage += '💮═══════════════💮\n';
         });
-        helpMessage += '💬https://facebook.com/OctobotRemake.Owner';
+        helpMessage += '💬https://www.facebook.com/Yhana.alvarez.445';
         api.sendMessage(helpMessage, event.threadID);
     }
 };
